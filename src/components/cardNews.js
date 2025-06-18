54fb6d88e8dd0c204b7e950252b4dd876d98550d
kmiiss <piresdesouza97@gmail.com>
2025-06-18 15:57:14 -0300

    construindo as props do cardNews

---
+++ src/components/cardNews.js
@@ -0,0 +1,49 @@
+class cardNews extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
+
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class","card");
+      
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card-left");
+
+    const autor = document.createElement("span");
+    autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+
+    const linkTitle = document.createElement("a");
+    linkTitle.textContent = this.getAttribute("title");
+
+    const newsContent = document.createElement("p");
+    newsContent.textContent = this.getAttribute("content");
+
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card-right");
+    const newsImage = document.createElement("img");
+    cardRight.appendChild(newsImage);
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+    }    
+    
+  styles() {
+        const style = document.createElement("style");
+
+        return style;
+    }
+}
+
+customElements.define("card-news", cardNews);
