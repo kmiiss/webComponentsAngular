ab1abca0c5aca23ddad2f7743680da9c589f6275
kmiiss <piresdesouza97@gmail.com>
2025-06-18 11:23:15 -0300

    construindo component profissional
    Construindo um component de maneira profissional

--- src/components/cardNew.js
+++ src/components/cardNew.js
@@ -3,9 +3,12 @@ class cardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World!</h1>"
 
     }
+
+    build() {}    
+    
+    styles() {}
 }
 
 customElements.define('card-news', cardNews);
